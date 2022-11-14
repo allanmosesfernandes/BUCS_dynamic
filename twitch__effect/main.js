6389a9fdaea61b2d1c30d6a6b5229d8c4b64b012
allanmosesfernandes <fernzallan14@gmail.com>
2022-11-14 10:45:09 +0000

    small-change

--- twitch__effect/main.js
+++ twitch__effect/main.js
@@ -9,48 +9,8 @@ subtitle.map((sub, index) => {
       wordSpan.style.transitionDelay = `${index * 40}ms`;
       wordSpan.innerHTML=`${word}`;
       sub.append(wordSpan);
-      console.log(wordSpan);
-      // return wordSpan.innerHTML=`${word}`
-      // wordSpan.classList.add("card-subtitle-word");
-      // wordSpan.style.transitionDelay = `${index * 40}ms`;
   })
-  // word.classList.add("card-subtitle-word");
-  // word.style.transitionDelay = `${index * 40}ms`;
-  // sub.innerText = "";
-  // return sub.append(subtitleWords);
+
 })
 
 
-
-const createSubs = () => {
-
-}
-
-// const createSpanWords = (text, index) => {
-//     const word = document.createElement("span");
-//     word.innerHTML = `${text}`;
-//     word.classList.add("card-subtitle-word");
-//     word.style.transitionDelay = `${index * 40}ms`;
-//     return word;
-    
-// }
-
-
-//Add the span tag to the DOM/Subtitle tag
-
-const addWords = (text,index) => subtitle.append(createSpanWords(text,index));
-
-
-//const AddDom = (text, index) => 
-
-// 
-const createSubtitle = text => text.split(" ").map(addWords);
-
-// createSubtitle("Find out communities and groups you can be a part of");
-// createSubtitle("Find out communities and groups you can be a part of");
-// createSubtitle("Find out communities and groups you can be a part of");
-// createSubtitle("Find out communities and groups you can be a part of");
-// createSubtitle("Find out communities and groups you can be a part of");
-// createSubtitle("Find out communities and groups you can be a part of");
-// createSubtitle("Find out communities and groups you can be a part of");
-// createSubtitle("Find out communities and groups you can be a part of");
